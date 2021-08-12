e46effceb94ad597aaa18ca70a033a24653d787c
Bechir <57349735+MohamedBechir@users.noreply.github.com>
2021-08-12 02:31:47 +0200

    file upload feature

---
+++ src/main/java/swiftparser/SwiftparserApplication.java
@@ -0,0 +1,20 @@
+package swiftparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import swiftparser.fileUpload.property.FileStorageProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties({
+	FileStorageProperties.class
+	})
+	@EnableJpaAuditing
+public class SwiftparserApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftparserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swiftparser/swiftparser/SwiftparserApplicationTests.java
@@ -0,0 +1,13 @@
+package swiftparser.swiftparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
