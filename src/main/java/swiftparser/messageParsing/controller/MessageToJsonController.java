7a71a599a3c9a549279e5d6672f77eedbf410965
MohamedBechir <jamoussi.bechir@yahoo.com>
2021-09-16 12:38:54 +0200

    convert list of messages to JSON Array

--- src/main/java/swiftparser/messageParsing/controller/MessageToJsonController.java
+++ src/main/java/swiftparser/messageParsing/controller/MessageToJsonController.java
@@ -1,5 +1,7 @@
 package swiftparser.messageParsing.controller;
 
+import java.util.List;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import swiftparser.messageParsing.payload.MessageDetailedInfoModel;
+import swiftparser.messageParsing.repository.MessageRepository;
 import swiftparser.messageParsing.service.MessageToJsonService;
 
 @RestController
@@ -18,22 +22,37 @@ import swiftparser.messageParsing.service.MessageToJsonService;
 public class MessageToJsonController {
 
     @Autowired
+    MessageRepository messageRepository;
+
+    @Autowired
     MessageToJsonService messageToJsonService;
 
     @Autowired
     private JmsTemplate jmsTemplate;
 
-    @PostMapping("messages/send/{id}/toJson")
+    @PostMapping("/messages/send/{id}/toJson")
     public Object sendJSONToQueue(@PathVariable Integer id) throws JsonProcessingException{
         try{
-            String Json = messageToJsonService.convertToJson(Long.valueOf(id));
+            String Json = messageToJsonService.convertMessageToJson(Long.valueOf(id));
             jmsTemplate.convertAndSend("DEV.QUEUE.2", Json);
             return  Json;
         }catch(JmsException ex){
             ex.printStackTrace();
             return "FAIL";
         }
-    } 
+    }
+
+    @GetMapping("/messages/tojson")
+    public List<MessageDetailedInfoModel> convertMessagesToJson() throws JsonProcessingException{
+        List<MessageDetailedInfoModel> JsonArray=null;
+        try{
+           JsonArray = messageToJsonService.convertMessagesToJson();
+        }catch(JmsException ex){
+            ex.printStackTrace();
+        }
+        return JsonArray;
+    }
+
     @GetMapping("messages/receive")
     Object ReceiveJSONFromQueue(){
     try{
--- src/main/java/swiftparser/messageParsing/service/MessageToJsonService.java
+++ src/main/java/swiftparser/messageParsing/service/MessageToJsonService.java
@@ -1,6 +1,9 @@
 package swiftparser.messageParsing.service;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -8,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import swiftparser.messageParsing.model.AbstractSwiftMessage;
+import swiftparser.messageParsing.payload.MessageDetailedInfoModel;
 import swiftparser.messageParsing.repository.MessageRepository;
 
 @Service
@@ -16,9 +20,21 @@ public class MessageToJsonService {
     @Autowired
     MessageRepository messageRepository;
 
-    public String convertToJson(Long id) throws JsonProcessingException{
+    public String convertMessageToJson(Long id) throws JsonProcessingException{
         ObjectMapper objectMapper = new ObjectMapper();
         AbstractSwiftMessage abstractSwiftMessage = messageRepository.findById(id).get();
         return objectMapper.writeValueAsString(abstractSwiftMessage);
     }
+
+    public List<MessageDetailedInfoModel> convertMessagesToJson() throws JsonProcessingException{
+        List<AbstractSwiftMessage> abstractSwiftMessage = messageRepository.findAll();
+        List<MessageDetailedInfoModel> jsonArray = new ArrayList<>();
+        for (AbstractSwiftMessage aSwiftMessage : abstractSwiftMessage) {
+            jsonArray.add(new MessageDetailedInfoModel(aSwiftMessage.getBlock1(), aSwiftMessage.getBlock2()
+            , aSwiftMessage.getTagBlock()));
+        }
+        return jsonArray;
+    }
+
+
 }
